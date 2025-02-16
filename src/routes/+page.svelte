0deccac1edc8a02b79e285fa27d26fad2c4cf0cd
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-02-15 20:38:40 -0600

    rearrange dynamic sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,77 +3,85 @@
 
 </script>
 
-<main class="relative pt-16 bg-zinc-900 w-full h-screen">
+<main class="relative pt-16 bg-zinc-900">
 
-    <div class="
-        absolute top-0 left-0 z-[1] w-dvw h-dvh
+    <div class="h-dvh">
+        
+        <div class="
+        absolute top-0 left-0 z-[1] w-full h-dvh
         bg-[url('$lib/noise.svg')] opacity-40
-    "></div>
+        "></div>
 
-    <div class="absolute top-0 left-0 z-[1] w-dvw h-dvh">
-        <Blob class="
-            bg-accent-300
-            w-2/6 h-2/5 max-w-lg
-            absolute top-12 left-14"
-        />
+        <div class="absolute top-0 left-0 z-[1] w-full h-dvh">
+            <Blob class="
+                bg-accent-300
+                w-2/6 h-2/5 max-w-lg
+                absolute top-12 left-14"
+            />
 
-        <Blob class="
-            pink-gradient 
-            w-5/6 h-3/5 max-w-lg
-            xs:w-11/12
-            absolute top-12 -left-10"
-        />
+            <Blob class="
+                pink-gradient 
+                w-5/6 h-3/5 max-w-lg
+                xs:w-11/12
+                absolute top-12 -left-10"
+            />
 
-        <Blob class="
-            pink-gradient
-            w-5/6 h-3/6 max-w-lg
-            xs:w-11/12
-            absolute top-52 -left-40"
-        />
+            <Blob class="
+                pink-gradient
+                w-5/6 h-3/6 max-w-lg
+                xs:w-11/12
+                absolute top-52 -left-40"
+            />
 
-        <Blob class="
-            pink-gradient
-            w-2/6 h-2/5 max-w-sm
-            absolute top-64 left-24"
-        />
+            <Blob class="
+                pink-gradient
+                w-2/6 h-2/5 max-w-sm
+                absolute top-64 left-24"
+            />
+        </div>
+
+
+        <div class="relative z-[2] px-[15%] sm:pt-[50px]" >
+
+            <div class="
+            pt-8 leading-tight tracking-widest
+            text-text-50 text-5xl text-left
+            "
+            >
+                Diego Varela
+            </div>
+
+            <h1 class="
+            mt-8
+            text-text-50 text-xl font-light 
+            lg:max-w-[600px]"
+            >
+                I'm a backend developer with 2+ years of experience
+                architecturing systems and software 
+                as a hobby
+            </h1>
+
+            <div class="
+            mt-20 text-white 
+            flex flex-row flex-wrap justify-between gap-x-8 gap-y-8
+            max-w-[600px]
+            ">
+                <div>
+                    <h1 class="text-base">What i'm doing</h1>
+                    <p class="text-white text-sm leading-5 opacity-50">studying systems engineering</p>
+                </div>
+
+                <div>
+                    <h1 class="text-base">What i like to do</h1>
+                    <p class="text-white text-sm leading-5 opacity-50">Use tech for a social good</p>
+                </div>
+            </div>
+
+        </div>
+    
     </div>
 
-    
-    <div class="relative z-[2] px-[15%] sm:pt-[50px]" >
-
-        <div class="
-        pt-8 leading-tight tracking-widest
-        text-text-50 text-5xl text-left
-        "
-        >
-            Diego Varela
-        </div>
-
-        <h1 class="
-        mt-8
-        text-text-50 text-xl font-light 
-        lg:max-w-[600px]"
-        >
-            I'm a backend developer with 2+ years of experience
-            architecturing systems and software 
-            as a hobby
-        </h1>
-
-        <div class="
-        mt-20 text-white 
-        flex flex-row flex-wrap justify-between gap-x-8 gap-y-8
-        max-w-[600px]
-        ">
-            <div>
-                <h1 class="text-base">What i'm doing</h1>
-                <p class="text-white text-sm leading-5 opacity-50">studying systems engineering</p>
-            </div>
-
-            <div>
-                <h1 class="text-base">What i like to do</h1>
-                <p class="text-white text-sm leading-5 opacity-50">Use tech for a social good</p>
-            </div>
-        </div>
+    <div class="h-dvh bg-zinc-900">
 
     </div>
 
