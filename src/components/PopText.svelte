6155e661bd35bf54a11af90ac6776d4d1c3f088c
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-02-17 18:55:04 -0600

    Separate pop up logic and change font sizes in diff screens

--- src/components/PopText.svelte
+++ src/components/PopText.svelte
@@ -8,15 +8,30 @@
 
     let observer;
 
+    function popText(){
+        texttag.classList.remove("-bottom-80");
+        texttag.classList.remove("opacity-0");
+        
+        texttag.classList.add("bottom-0");
+        texttag.classList.add("opacity-50");
+    }
+
     function handleIntersect(entries, observer){
         entries.forEach(entry => {
             if(entry.isIntersecting) {
                 //element in screen
-                texttag.classList.remove("-bottom-80");
-                texttag.classList.add("bottom-0");
-
+                popText();
                 observer.disconnect();
             }
+            // else{
+                
+            //     texttag.classList.remove("bottom-0");
+            //     texttag.classList.remove("opacity-50");
+            //     texttag.classList.add("-bottom-80");
+            //     texttag.classList.add("opacity-0");
+                
+            // }
+
         });
     }
 
@@ -31,11 +46,12 @@
 
 <h1 bind:this={texttag} class=" {className}
     w-full
-    text-5xl sm:text-8xl font-black opacity-50
+    text-4xl sm:text-5xl font-black 
     text-center tracking-widest
     absolute -bottom-80 left-0
-    
-    transition-all duration-500 ease-in-out
+    opacity-0 
+
+    transition-all duration-700 ease-in-out
 
 ">
     {text}
